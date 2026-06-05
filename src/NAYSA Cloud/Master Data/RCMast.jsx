44828780784a94986d09f5afa602b98a1dbd6f67
Merie Anjeaneth Alarcon <aalarcon.naysa@gmail.com>
2026-06-05 18:11:00 +0800

    Resolved QC File - Reference Files

--- src/NAYSA Cloud/Master Data/RCMast.jsx
+++ src/NAYSA Cloud/Master Data/RCMast.jsx
@@ -334,8 +334,8 @@ const RCMast = () => {
           </div>
         ),
       },
-      { key: "rcCode", label: "RC Code", sortable: true, width: 120 },
-      { key: "rcName", label: "RC Name", sortable: true, width: 350, maxWidth: 350 },
+      { key: "rcCode", label: "RC Code", sortable: true, width: 120, requiredVisible: true  },
+      { key: "rcName", label: "RC Name", sortable: true, width: 350, maxWidth: 350, requiredVisible: true  },
       {
         key: "rcTypeCode",
         label: "RC Type",
